8cacee4e7a1512c984d5c72171249b3a370089c4
pb-coding <71174645+pb-coding@users.noreply.github.com>
2022-09-04 21:18:42 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './index.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-3xl font-bold underline text-green-400">
+      <h1>VITE + Tailwind</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
