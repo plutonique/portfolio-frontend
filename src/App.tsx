e65b9531d50615bce900dab5b0e76b8a665351c7
pb-coding <71174645+pb-coding@users.noreply.github.com>
2022-09-06 16:03:15 +0200

    adds ci.yml for github actions
    adds ci.yml for github actions
    
    removes unsupported node version

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App text-3xl font-bold underline text-green-400">
-      <h1>VITE + Tailwind</h1>
+    <div className="App text-3xl font-bold underline text-blue-700">
+      <h1>VITE + Tailwind = awesome</h1>
     </div>
   )
 }
